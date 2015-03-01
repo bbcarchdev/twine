f01ba025980e8e0001ba85963cdcda103e783ed0
Mo McRoberts <mo.mcroberts@bbc.co.uk>
2015-03-01 16:54:31 +0000

    Properly sign x-amz-* request headers

--- libs3client/p_libs3client.h
+++ libs3client/p_libs3client.h
@@ -25,6 +25,7 @@
 # include <string.h>
 # include <time.h>
 # include <errno.h>
+# include <ctype.h>
 
 # include <curl/curl.h>
 # include <openssl/hmac.h>
--- libs3client/sign.c
+++ libs3client/sign.c
@@ -1,6 +1,6 @@
 /* Author: Mo McRoberts <mo.mcroberts@bbc.co.uk>
  *
- * Copyright (c) 2014 BBC
+ * Copyright (c) 2014-2015 BBC
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -21,6 +21,8 @@
 
 #include "p_libs3client.h"
 
+static int s3_header_sort_(const void *a, const void *b);
+
 static char *
 stradd(char *dest, const char *src)
 {
@@ -32,9 +34,10 @@ stradd(char *dest, const char *src)
 struct curl_slist *
 s3_sign(const char *method, const char *resource, const char *access_key, const char *secret, struct curl_slist *headers)
 {
-	const char *type, *md5, *date, *adate;
-	size_t len;
-	char *t, *buf;
+	const char *type, *md5, *date, *adate, *hp;
+	size_t len, amzlen, amzcount, c, l;
+	char *t, *s, *buf, *amzbuf;
+	char **amzhdr;
 	struct curl_slist *p;
 	unsigned char digest[20];
 	unsigned digestlen;
@@ -47,7 +50,7 @@ s3_sign(const char *method, const char *resource, const char *access_key, const 
 
 	type = md5 = date = adate = NULL;
 	len = strlen(method) + strlen(resource) + 2;
-
+	amzcount = 0;
 	for(p = headers; p; p = p->next)
 	{
 		if(!strncasecmp(p->data, "content-type: ", 14))
@@ -68,7 +71,8 @@ s3_sign(const char *method, const char *resource, const char *access_key, const 
 		}
 		else if(!strncasecmp(p->data, "x-amz-", 6))
 		{
-			len += strlen(p->data) + 1;
+			amzlen += strlen(p->data) + 1;
+			amzcount++;
 		}
 	}
 	if(adate)
@@ -93,7 +97,7 @@ s3_sign(const char *method, const char *resource, const char *access_key, const 
 		headers = curl_slist_append(headers, datebuf);
 		date = &(datebuf[6]);
 	}
-	len += strlen(type) + strlen(md5) + strlen(date) + 2;
+	len += amzlen + strlen(type) + strlen(md5) + strlen(date) + 2;
 	buf = (char *) calloc(1, len + 1);
 	if(!buf)
 	{
@@ -111,9 +115,70 @@ s3_sign(const char *method, const char *resource, const char *access_key, const 
 	t = stradd(t, date);
 	*t = '\n';
 	t++;
-
+	if(amzcount)
+	{
+		fprintf(stderr, "*** have x-amz-* headers\n");
+		/* Build an array of x-amz-* headers, excluding x-amz-date */
+		amzhdr = (char **) calloc(amzcount, sizeof(char *));
+		amzbuf = (char *) calloc(1, amzlen + 1);
+		s = amzbuf;
+		amzcount = 0;
+		for(p = headers; p; p = p->next)
+		{
+			if(!strncasecmp(p->data, "x-amz-date: ", 12))
+			{
+				continue;
+			}
+			else if(!strncasecmp(p->data, "x-amz-", 6))
+			{
+				amzhdr[amzcount] = s;
+				amzcount++;
+				for(hp = p->data; *hp; hp++)
+				{
+					if(*hp == ':')
+					{
+						*s = ':';
+						s++;
+						hp++;
+						while(isspace(*hp))
+						{
+							hp++;
+						}
+						strcpy(s, hp);
+						s = strchr(s, 0);
+						break;
+					}
+					*s = tolower(*hp);
+					s++;
+					*s = 0;
+				}
+				s++;
+			}
+		}
+		qsort(amzhdr, amzcount, sizeof(char *), s3_header_sort_);
+		for(c = 0; c < amzcount; c++)
+		{			
+			hp = strchr(amzhdr[c], ':');
+			if(!hp)
+			{
+				continue;
+			}
+			l = hp - amzhdr[c];
+			t = stradd(t, amzhdr[c]);
+			while(c + 1 < amzcount && !strncmp(amzhdr[c + 1], amzhdr[c], l))
+			{
+				c++;
+				*t = ',';
+				t++;
+				t = stradd(t, amzhdr[c] + l + 1);
+			}
+			*t = '\n';
+			t++;
+		}
+		free(amzbuf);
+		free(amzhdr);
+	}
 	t = stradd(t, resource);
-
 	HMAC(EVP_sha1(), secret, strlen(secret), (unsigned char *) buf, strlen(buf), digest, &digestlen);
 	
 	free(buf);
@@ -136,4 +201,15 @@ s3_sign(const char *method, const char *resource, const char *access_key, const 
 	BIO_free_all(b64);
 	free(sigbuf);
 	return headers;
+}
+
+static int
+s3_header_sort_(const void *a, const void *b)
+{
+	const char **stra, **strb;
+
+	stra = (const char **) a;
+	strb = (const char **) b;
+
+	return strcmp(*stra, *strb);
 }
