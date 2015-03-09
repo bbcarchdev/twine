f0b6836bf4fa21c2b9508fffc6c91e6de4f4d9f2
Mo McRoberts <mo.mcroberts@bbc.co.uk>
2015-03-09 12:59:59 +0000

    Add -A (process all), -l (list URLs only) and -n (don't update status in queue database) options.

--- bridges/anansi-bridge.c
+++ bridges/anansi-bridge.c
@@ -36,7 +36,7 @@
 #define ANANSI_URL_TYPE                 "application/x-anansi-url"
 
 static void usage(void);
-static int anansi_runloop(MQ *messenger, SQL *sql, const char *bucket);
+static int anansi_runloop(MQ *messenger, SQL *sql, const char *bucket, int fetch_all, int list_only, int no_complete);
 
 int
 main(int argc, char **argv)
@@ -44,8 +44,11 @@ main(int argc, char **argv)
 	const char *dburi, *bucket;
 	SQL *sql;
 	MQ *messenger;
-	int r, c;
+	int r, c, fetch_all, list_only, no_complete = 0;
 
+	fetch_all = 0;
+	list_only = 0;
+	no_complete = 0;
 	if(utils_init(argc, argv, 0))
 	{
 		return 1;
@@ -54,7 +57,7 @@ main(int argc, char **argv)
 	{
 		return 1;
 	}
-	while((c = getopt(argc, argv, "hdc:t:s:")) != -1)
+	while((c = getopt(argc, argv, "hdc:t:s:Aln")) != -1)
 	{
 		switch(c)
 		{
@@ -66,6 +69,15 @@ main(int argc, char **argv)
 			break;
 		case 'c':
 			config_set("global:configFile", optarg);
+			break;
+		case 'A':
+			fetch_all = 1;
+			break;
+		case 'l':
+			list_only = 1;
+			break;
+		case 'n':
+			no_complete = 1;
 			break;
 		default:
 			usage();
@@ -86,11 +98,18 @@ main(int argc, char **argv)
 	if(utils_mq_init_send(TWINE_APP_NAME ":mq"))
 	{
 		return 1;
-	}	
-	messenger = utils_mq_messenger();
-	if(!messenger)
+	}
+	if(list_only)
 	{
-		return 1;
+		messenger = NULL;
+	}
+	else
+	{
+		messenger = utils_mq_messenger();
+		if(!messenger)
+		{
+			return 1;
+		}
 	}
 	dburi = config_getptr_unlocked(TWINE_APP_NAME ":db", "mysql://localhost/anansi");
 	sql = sql_connect(dburi);
@@ -101,11 +120,14 @@ main(int argc, char **argv)
 	}
 	bucket = config_getptr_unlocked(TWINE_APP_NAME ":bucket", "anansi");
 
-	r = anansi_runloop(messenger, sql, bucket);
+	r = anansi_runloop(messenger, sql, bucket, fetch_all, list_only, no_complete);
 
 	sql_disconnect(sql);
 
-	mq_disconnect(messenger);
+	if(!list_only)
+	{
+		mq_disconnect(messenger);
+	}
 	
 	return (r ? 1 : 0);
 }
@@ -118,17 +140,20 @@ usage(void)
 			"OPTIONS is one or more of:\n\n"
 			"  -h                   Print this notice and exit\n"
 			"  -d                   Enable debug output\n"
+			"  -A                   Fetch all entries, rather than only new ones\n"
+			"  -l                   Write entry URLs to standard output instead of queueing\n"
+			"  -n                   Don't mark entries as complete once queued\n"
 			"\n",
 			utils_progname);
 }
 
 static int 
-anansi_runloop(MQ *messenger, SQL *sql, const char *bucket)
+anansi_runloop(MQ *messenger, SQL *sql, const char *bucket, int fetch_all, int list_only, int no_complete)
 {
 	MQMESSAGE *message;
 	SQL_STATEMENT *rs;
 	char *uribuf, *p;
-	int printed;
+	int printed, offset;
 
 	uribuf = (char *) calloc(1, 8 + strlen(bucket) + 64);
 	if(!uribuf)
@@ -141,9 +166,23 @@ anansi_runloop(MQ *messenger, SQL *sql, const char *bucket)
 	strcat(uribuf, "/");
 	p = strchr(uribuf, 0);
 	printed = 0;
+	offset = 0;
+	if(fetch_all && !no_complete)
+	{
+		log_printf(LOG_DEBUG, "marking previously-complete entries as new\n");
+		sql_executef(sql, "UPDATE \"crawl_resource\" SET \"state\" = %Q WHERE \"state\" = %Q", "ACCEPTED", "COMPLETE");
+	}
+		
 	for(;;)
 	{
-		rs = sql_queryf(sql, "SELECT \"hash\" FROM \"crawl_resource\" WHERE \"state\" = %Q ORDER BY \"updated\" DESC LIMIT 5", "ACCEPTED");
+		if(fetch_all && no_complete)
+		{
+			rs = sql_queryf(sql, "SELECT \"hash\" FROM \"crawl_resource\" WHERE \"state\" IN (%Q, %Q) ORDER BY \"updated\" DESC LIMIT 5 OFFSET %d", "ACCEPTED", "COMPLETE", offset);
+		}
+		else
+		{
+			rs = sql_queryf(sql, "SELECT \"hash\" FROM \"crawl_resource\" WHERE \"state\" = %Q ORDER BY \"updated\" DESC LIMIT 5 OFFSET %d", "ACCEPTED", offset);
+		}
 		if(!rs)
 		{
 			log_printf(LOG_CRIT, "query for updated resources failed\n");
@@ -153,6 +192,10 @@ anansi_runloop(MQ *messenger, SQL *sql, const char *bucket)
 		{
 			if(!printed)
 			{
+				if(list_only)
+				{
+					break;
+				}
 				log_printf(LOG_DEBUG, "no new resources remain; sleeping\n");
 				printed = 1;
 			}
@@ -169,29 +212,46 @@ anansi_runloop(MQ *messenger, SQL *sql, const char *bucket)
 				sql_stmt_destroy(rs);
 				return -1;
 			}
-			log_printf(LOG_DEBUG, "URI is <%s>\n", uribuf);
-			if(!(message = mq_message_create(messenger)))
+			if(list_only)
 			{
-				log_printf(LOG_CRIT, "failed to create new message\n");
-				sql_stmt_destroy(rs);
-				return -1;
-			}			
-			mq_message_set_type(message, ANANSI_URL_TYPE);
-			mq_message_add_bytes(message, (unsigned char *) uribuf, strlen(uribuf));
-			if(mq_message_send(message))
-			{
-				log_printf(LOG_CRIT, "failed to send message: %s\n", mq_errmsg(messenger));
-				sql_stmt_destroy(rs);
-				return -1;
+				printf("%s\n", uribuf);
 			}
-			sql_executef(sql, "UPDATE \"crawl_resource\" SET \"state\" = %Q WHERE \"hash\" = %Q AND \"state\" = %Q", "COMPLETE", p, "ACCEPTED");
-			if(mq_deliver(messenger))
+			else
 			{
-				log_printf(LOG_CRIT, "failed to deliver message: %s\n", mq_errmsg(messenger));
-				sql_stmt_destroy(rs);
-				return -1;
+				log_printf(LOG_DEBUG, "URI is <%s>\n", uribuf);
+				if(!(message = mq_message_create(messenger)))
+				{
+					log_printf(LOG_CRIT, "failed to create new message\n");
+					sql_stmt_destroy(rs);
+					return -1;
+				}
+				mq_message_set_type(message, ANANSI_URL_TYPE);
+				mq_message_add_bytes(message, (unsigned char *) uribuf, strlen(uribuf));
+				if(mq_message_send(message))
+				{
+					log_printf(LOG_CRIT, "failed to send message: %s\n", mq_errmsg(messenger));
+					sql_stmt_destroy(rs);
+					return -1;
+				}
 			}
-			mq_message_free(message);
+			if(no_complete)
+			{
+				offset++;
+			}
+			else
+			{
+				sql_executef(sql, "UPDATE \"crawl_resource\" SET \"state\" = %Q WHERE \"hash\" = %Q AND \"state\" = %Q", "COMPLETE", p, "ACCEPTED");
+			}
+			if(!list_only)
+			{
+				if(mq_deliver(messenger))
+				{
+					log_printf(LOG_CRIT, "failed to deliver message: %s\n", mq_errmsg(messenger));
+					sql_stmt_destroy(rs);
+					return -1;
+				}
+				mq_message_free(message);
+			}
 			sql_stmt_next(rs);
 		}
 		sql_stmt_destroy(rs);
