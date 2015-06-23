623c6b44f9f44cf97685e3a00b9ec02cf3e4fe8d
Mo McRoberts <mo.mcroberts@bbc.co.uk>
2015-06-23 12:54:15 +0000

    Remove bulk-import capabilities from twine-writerd -- use bindir/twine instead (closes #13)

--- writer/writerd.c
+++ writer/writerd.c
@@ -33,39 +33,16 @@ static int writerd_init(int argc, char **argv);
 static int writerd_process_args(int argc, char **argv);
 static int writerd_plugin_config_cb(const char *key, const char *value, void *data);
 static void writerd_signal(int sig);
-static int writerd_import(const char *type, const char *filename);
-
-static const char *bulk_import = NULL, *bulk_import_file = NULL;
-
-static struct extmime extmime[] = {
-	{ "trig", "application/trig" },
-	{ "nq", "application/n-quads" },
-	{ "xml", "text/xml" },
-	{ "ttl", "text/turtle" },
-	{ "rdf", "application/rdf+xml" },
-	{ "html", "text/html" },
-	{ "txt", "text/plain" },
-	{ "json", "application/json" },
-	{ "nt", "application/n-triples" },
-	{ NULL, NULL }
-};
 
 int
 main(int argc, char **argv)
 {
 	pid_t child;
 	int detach;
-	int r;
 
 	if(writerd_init(argc, argv))
 	{
 		return 1;
-	}
-	if(bulk_import)
-	{
-		r = writerd_import(bulk_import, bulk_import_file);
-		twine_cleanup_();
-		return r ? 1 : 0;
 	}
 	detach = config_get_bool(TWINE_APP_NAME ":detach", 1);
 	signal(SIGHUP, SIG_IGN);
@@ -151,13 +128,10 @@ writerd_init(int argc, char **argv)
 	{
 		return -1;
 	}
-	if(!bulk_import)
+	/* Set up the AMQP interface */
+	if(utils_mq_init_recv(TWINE_APP_NAME ":mq"))
 	{
-		/* Set up the AMQP interface */
-		if(utils_mq_init_recv(TWINE_APP_NAME ":mq"))
-		{
-			return -1;
-		}
+		return -1;
 	}
 	/* Load plug-ins */
 	if(config_get_all(TWINE_APP_NAME, "module", writerd_plugin_config_cb, NULL))
@@ -177,7 +151,6 @@ writerd_usage(void)
 			"  -f                   Don't detach and run in the background\n"
 			"  -d                   Enable debug output to standard error\n"
 			"  -c FILE              Specify path to configuration file\n"
-			"  -t TYPE              Perform a bulk import from stdin of TYPE\n"
 			"\n",
 			utils_progname);
 }
@@ -186,8 +159,6 @@ static int
 writerd_process_args(int argc, char **argv)
 {
 	int c;
-	size_t n;
-	const char *t;
 
 	while((c = getopt(argc, argv, "hfc:dt:")) != -1)
 	{
@@ -209,16 +180,6 @@ writerd_process_args(int argc, char **argv)
 			config_set("s3:verbose", "1");
 			config_set(TWINE_APP_NAME ":detach", "0");
 			break;
-		case 't':
-			if(bulk_import)
-			{
-				fprintf(stderr, "%s: cannot specify multiple import formats ('%s' versus '%s')\n", utils_progname, bulk_import, optarg);
-				return -1;
-			}
-			bulk_import = optarg;
-			config_set("log:stderr", "1");
-			config_set(TWINE_APP_NAME ":detach", "0");
-			break;
 		default:
 			writerd_usage();
 			return -1;
@@ -226,35 +187,6 @@ writerd_process_args(int argc, char **argv)
 	}
 	argc -= optind;
 	argv += optind;
-	if(argc == 1)
-	{
-		config_set("log:stderr", "1");
-		config_set(TWINE_APP_NAME ":detach", "0");
-		if(!bulk_import)
-		{
-			t = strrchr(argv[0], '.');
-			if(t)
-			{
-				t++;
-				for(n = 0; extmime[n].ext; n++)
-				{
-					if(!strcasecmp(extmime[n].ext, t))
-					{
-						bulk_import = extmime[n].mime;
-						break;
-					}
-				}
-			}
-		}
-		if(!bulk_import)
-		{
-			fprintf(stderr, "%s: the MIME type of '%s' cannot be automatically determined; specify it with '-t TYPE'\n", utils_progname, argv[0]);
-			return -1;
-		}
-		bulk_import_file = argv[0];
-		argc--;
-		argv++;
-	}
 	if(argc)
 	{
 		/* There should not be any remaining command-line arguments */
@@ -283,103 +215,4 @@ writerd_signal(int signo)
 	(void) signo;
 	
 	writerd_exit();
-}
-
-static int
-writerd_import(const char *type, const char *filename)
-{
-	FILE *f;
-	unsigned char *buffer, *p;
-	size_t buflen, bufsize;
-	ssize_t r;
-
-	if(filename)
-	{
-		f = fopen(filename, "rb");
-		if(!f)
-		{
-			twine_logf(LOG_CRIT, "%s: %s\n", filename, strerror(errno));
-			return -1;
-		}
-	}
-	else
-	{
-		f = stdin;
-	}
-	if(twine_bulk_supported(type))
-	{
-		r = twine_bulk_import(type, f);
-		if(filename)
-		{
-			fclose(f);
-		}
-		return (r ? -1 : 0);
-	}
-	if(!twine_plugin_supported(type))
-	{
-		twine_logf(LOG_CRIT, "no registered plug-in supports the MIME type '%s'\n", type);
-		if(filename)
-		{
-			fclose(f);
-		}
-		return -1;
-	}
-	if(filename)
-	{
-		twine_logf(LOG_INFO, "performing bulk import of '%s' from '%s'\n", type, filename);
-	}
-	else
-	{
-		twine_logf(LOG_INFO, "performing bulk import of '%s' from standard input\n", type);
-	}
-	/* Read file into the buffer, extending as needed */
-	buffer = NULL;
-	bufsize = 0;
-	buflen = 0;
-	while(!feof(f))
-	{
-		if(bufsize - buflen < 1024)
-		{
-			p = (unsigned char *) realloc(buffer, bufsize + 1024);
-			if(!p)
-			{
-				twine_logf(LOG_CRIT, "failed to reallocate buffer from %u bytes to %u bytes\n", (unsigned) bufsize, (unsigned) bufsize + 1024);
-				free(buffer);
-				return -1;
-			}
-			buffer = p;
-			bufsize += 1024;
-		}
-		r = fread(&(buffer[buflen]), 1, 1023, f);
-		if(r < 0)
-		{
-			if(filename)
-			{
-				twine_logf(LOG_CRIT, "error reading from '%s': %s\n", filename, strerror(errno));
-			}
-			else
-			{
-				twine_logf(LOG_CRIT, "error reading from standard input: %s\n", strerror(errno));
-			}
-			free(buffer);
-			return -1;
-		}
-		buflen += r;
-		buffer[buflen] = 0;
-	}
-	r = twine_plugin_process(type, buffer, buflen, NULL);
-	if(r)
-	{
-		twine_logf(LOG_CRIT, "failed to process input as '%s'\n", type);
-	}
-	else
-	{
-		twine_logf(LOG_NOTICE, "successfully imported data as '%s'\n", type);
-	}
-	free(buffer);
-	if(filename)
-	{
-		fclose(f);
-	}
-	return (r ? -1 : 0);
 }
